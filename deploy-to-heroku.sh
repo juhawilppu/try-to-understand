39f4fbc3095ce162fd44b8ec7ce7c35d937ddca7
Juha Wilppu <juha.wilppu@gmail.com>
2019-10-31 00:10:20 +0200

    k

--- deploy-to-heroku.sh
+++ deploy-to-heroku.sh
@@ -1,1 +1,3 @@
 git push heroku master
+heroku run sequelize db:migrate --env production --app trytounderstand
+heroku run sequelize db:seed:all --env production --app trytounderstand
--- migrate-production.js
+++
@@ -1,3 +0,0 @@
-heroku run sequelize db:migrate --env production --app trytounderstand
-heroku run sequelize db:seed:all --env production --app trytounderstand
-
