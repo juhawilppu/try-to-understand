e768d46c75cef9a6e61352a810c0813f325a46c2
Juha Wilppu <juha.wilppu@gmail.com>
2019-12-15 23:07:47 +0200

    Add types to Admin page

--- client/src/views/Admin.tsx
+++ client/src/views/Admin.tsx
@@ -2,11 +2,25 @@ import * as React from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 import TButton from 'components/TButton';
+import { RouteComponentProps } from "react-router-dom";
 
-class Admin extends React.Component<any> {
+interface Word {
+    id: number;
+    language: string;
+    word: "english" | "swedish";
+}
+
+interface Props extends RouteComponentProps {
+
+}
+interface State {
+    loaded: boolean;
+    words: Word[] | null;
+}
+class Admin extends React.Component<Props, State> {
     state = {
         loaded: false,
-        words: []
+        words: null
     }
 
     componentDidMount() {
@@ -15,7 +29,10 @@ class Admin extends React.Component<any> {
 
     load = () => {
         axios.get('/api/admin/words').then(response => {
-            this.setState({ loaded: true, words: response.data });
+            this.setState({
+                loaded: true,
+                words: response.data
+            });
         }).catch(error => alert(error));
     }
 
@@ -26,33 +43,44 @@ class Admin extends React.Component<any> {
     delete = (id : number) => {
         axios.delete(`/api/admin/words/${id}`).then(response => {
             this.load();
-        });
+        }).catch(error => alert(error));
     }
 
+    renderTable = (words : Word[]) => (
+        <table>
+            <thead>
+                <tr>
+                    <th>Language</th>
+                    <th>Word</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {words.map(exp =>
+                    <tr key={exp.id}>
+                        <td>{exp.language}</td>
+                        <td>{exp.word}</td>
+                        <td><TButton flat onClick={() => this.delete(exp.id)}>Delete</TButton></td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    )
+
     render() {
+
+        if (!this.state.loaded) {
+            return <div>Loading...</div>
+        }
+        
+        const words = this.state.words! as Word[];
+
         return (
             <div style={{width: '500px'}} className="explain-view">
                 <h2>Words</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Language</th>
-                            <th>Word</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.loaded && this.state.words.map((exp : any) =>
-                            <tr key={exp.id}>
-                                <td>{exp.language}</td>
-                                <td>{exp.word}</td>
-                                <td><TButton flat onClick={() => this.delete(exp._id)}>Delete</TButton></td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
+                {this.state.loaded && this.renderTable(words)}
                 <TButton onClick={this.gotoAddWord}>Add word</TButton>
-                {this.state.loaded && this.state.words.length === 0 && <div>No words.</div>}
+                {this.state.loaded && words.length === 0 && <div>No words.</div>}
                 {!this.state.loaded && <div>Loading</div>}
             </div>
         )
